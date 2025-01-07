562c26b5d26a1a9d214425c5b7d2def0f434b989
Nefrius tr <enesbas616161@gmail.com>
2025-01-07 17:35:13 +0300

    New Releases

--- cursorqr/app/qr-olustur/search-params-content.tsx
+++ cursorqr/app/qr-olustur/search-params-content.tsx
@@ -1,12 +1,21 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 import dynamic from 'next/dynamic'
+import { useSearchParams } from 'next/navigation'
 
 const QRContent = dynamic(() => import('./qr-content'), { ssr: false })
 
-export default function SearchParamsContent() {
+function SearchContent() {
   const searchParams = useSearchParams()
   const imageUrl = searchParams.get('imageUrl')
   return imageUrl ? <QRContent imageUrl={imageUrl} /> : null
+}
+
+export default function SearchParamsContent() {
+  return (
+    <Suspense>
+      <SearchContent />
+    </Suspense>
+  )
 } 

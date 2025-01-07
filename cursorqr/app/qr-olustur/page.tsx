06f1f57bdd8ad52c69be9e537d705e7b6cf51eb2
Nefrius tr <enesbas616161@gmail.com>
2025-01-07 17:32:50 +0300

    New Releases

--- cursorqr/app/qr-olustur/page.tsx
+++ cursorqr/app/qr-olustur/page.tsx
@@ -1,16 +1,7 @@
-'use client'
-
 import { Suspense } from 'react'
-import { useSearchParams } from 'next/navigation'
 import dynamic from 'next/dynamic'
 
-const QRContent = dynamic(() => import('./qr-content'), { ssr: false })
-
-function SearchParamsContent() {
-  const searchParams = useSearchParams()
-  const imageUrl = searchParams.get('imageUrl')
-  return imageUrl ? <QRContent imageUrl={imageUrl} /> : null
-}
+const SearchParamsContent = dynamic(() => import('./search-params-content'), { ssr: false })
 
 export default function QRPage() {
   return (
--- cursorqr/app/qr-olustur/search-params-content.tsx
+++ cursorqr/app/qr-olustur/search-params-content.tsx
@@ -2,17 +2,11 @@
 
 import { useSearchParams } from 'next/navigation'
 import dynamic from 'next/dynamic'
-import type { ComponentType } from 'react'
 
-interface QRContentProps {
-  imageUrl: string
-}
-
-const QRContent: ComponentType<QRContentProps> = dynamic(() => import('./qr-content').then(mod => mod.default), { ssr: false })
+const QRContent = dynamic(() => import('./qr-content'), { ssr: false })
 
 export default function SearchParamsContent() {
   const searchParams = useSearchParams()
   const imageUrl = searchParams.get('imageUrl')
-
   return imageUrl ? <QRContent imageUrl={imageUrl} /> : null
 } 

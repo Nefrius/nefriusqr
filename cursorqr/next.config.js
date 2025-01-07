de9554c4239b8b9a6cb42811cadf99600ad6f91e
Nefrius tr <enesbas616161@gmail.com>
2025-01-07 17:29:33 +0300

    New Releases

--- cursorqr/next.config.js
+++ cursorqr/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
-    domains: ['i.ibb.co', 'lh3.googleusercontent.com']
-  }
+    domains: ['i.ibb.co', 'lh3.googleusercontent.com'],
+    unoptimized: true
+  },
+  basePath: '/mynefriusqr',
+  assetPrefix: '/mynefriusqr/'
 }
 
 module.exports = nextConfig 
